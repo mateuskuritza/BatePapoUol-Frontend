4f8a10d907cef5e08c5f47d768df0864a365c78b
mateuskuritza <mateuskuritza@gmail.com>
2021-04-12 20:38:34 -0300

    login screen add

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,14 @@
 const sideMenuBackground = document.querySelector(".menu-background");
 const sideMenu = document.querySelector(".menu-choices");
-
-
+const loginScreen = document.querySelector(".login-screen");    
+let userName;
 function showMenu(){
     sideMenuBackground.classList.toggle("none");
     sideMenu.classList.toggle("none");
 }
+
+function TakeUserName(){
+    loginScreen.classList.add("none");
+    userName =  document.querySelector(".login-input").value;
+}
+
