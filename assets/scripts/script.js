843d6abcf44a3ea599eb584ef0534843ce63f159
mateuskuritza <mateuskuritza@gmail.com>
2021-04-12 21:07:42 -0300

    select sidemenu add

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,6 +2,7 @@ const sideMenuBackground = document.querySelector(".menu-background");
 const sideMenu = document.querySelector(".menu-choices");
 const loginScreen = document.querySelector(".login-screen");    
 let userName;
+
 function showMenu(){
     sideMenuBackground.classList.toggle("none");
     sideMenu.classList.toggle("none");
@@ -12,3 +13,15 @@ function TakeUserName(){
     userName =  document.querySelector(".login-input").value;
 }
 
+function select(element){
+    const type = "." + element.classList.value;
+    const selectedContacts = document.querySelectorAll(type + ".selected");
+
+    if(selectedContacts.length !== null){
+        for(let i = 0; i<selectedContacts.length; i++){
+            selectedContacts[i].classList.remove("selected");
+        }
+    }
+
+    element.classList.add("selected");
+}
